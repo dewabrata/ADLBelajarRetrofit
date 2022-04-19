26a3a94ed9ea440d17d62efb130412db44cb62e4
dewabrata <dewabrata@gmail.com>
2022-04-19 10:01:24 +0700

    Add Query pada service

--- app/src/main/java/com/adl/belajarretrofit/MainActivity.kt
+++ app/src/main/java/com/adl/belajarretrofit/MainActivity.kt
@@ -6,6 +6,7 @@ import android.util.Log
 import android.widget.Toast
 import com.adl.belajarretrofit.model.OMDBResponse
 import com.adl.belajarretrofit.services.RetrofitConfig
+import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -14,9 +15,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        RetrofitConfig().getAPIService().getDetailMovie().enqueue(object: Callback<OMDBResponse>{
+        RetrofitConfig().getAPIService().getDetailMovie("titanic").enqueue(object: Callback<OMDBResponse>{
             override fun onResponse(call: Call<OMDBResponse>, response: Response<OMDBResponse>) {
               Log.d("Response",response.toString())
+                val data: OMDBResponse? = response.body()
+
+                txtDirector.setText(data?.director)
+                txtTitle.setText(data?.title)
+
             }
 
             override fun onFailure(call: Call<OMDBResponse>, t: Throwable) {
--- app/src/main/java/com/adl/belajarretrofit/services/IMovieDetail.kt
+++ app/src/main/java/com/adl/belajarretrofit/services/IMovieDetail.kt
@@ -3,9 +3,11 @@ package com.adl.belajarretrofit.services
 import com.adl.belajarretrofit.model.OMDBResponse
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface IMovieDetail {
 
-    @GET("?t=titan&apikey=80641bfb")
-    fun getDetailMovie(): Call<OMDBResponse>
+    @GET("?apikey=80641bfb")
+    fun getDetailMovie(
+        @Query("t" ) query:String   ): Call<OMDBResponse>
 }
