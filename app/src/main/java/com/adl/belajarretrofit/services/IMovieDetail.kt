96645d06c567f4c3a359baf09d70ff23c814cb85
dewabrata <dewabrata@gmail.com>
2022-04-19 11:21:43 +0700

    Add themes

--- app/src/main/java/com/adl/belajarretrofit/services/IMovieDetail.kt
+++ app/src/main/java/com/adl/belajarretrofit/services/IMovieDetail.kt
@@ -1,6 +1,7 @@
 package com.adl.belajarretrofit.services
 
 import com.adl.belajarretrofit.model.OMDBResponse
+import com.adl.belajarretrofit.model.SearchResponse
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -10,4 +11,7 @@ interface IMovieDetail {
     @GET("?apikey=80641bfb")
     fun getDetailMovie(
         @Query("t" ) query:String   ): Call<OMDBResponse>
+
+    @GET("?apikey=80641bfb")
+    fun searchMovie(@Query("s" ) query:String) :Call<SearchResponse>
 }
